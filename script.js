cab0b33ff8aa6ae9cf4407c27ec01c7df74ee21b
Réza <reza@muretech.com>
2021-04-06 12:35:49 +0200

    now append result instead of replacing

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function inverser(text) {
+function invert(text) {
     const words = text.split(' ');
     console.log(words)
     const wordsRevesed = words.reverse();
@@ -7,23 +7,28 @@ function inverser(text) {
 }
 
 function start() {
+    $('#error').html();
     let text = $('#text').val().trim();
     const isDot = text.indexOf('.');
-    let result;
+    let result, source = $('#text').val();
     if (text === '.') {
         result = '.';
-        $('#text').val('');
     } else if (isDot > 0) {
         text = text.replace('.', '');
-        result = inverser(text) + '.';
-        $('#text').val('');
+        result = invert(text) + '.';
     }
-    if (result) $('#result').hide().html(result).fadeIn();
-    else $('#result').hide().html('<div class="text-danger">format invalide...</div>').fadeIn();
+
+    if (result) {
+        $('#text').val('');
+        const div = $('div');
+        $('#result').append(`<div>${source}&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;${result}</div>`);
+        $('#result div:last').hide().fadeIn();
+    }
+    else $('#error').hide().html('format invalide...').fadeIn();
 }
 
 $(document).ready(function() {
-    $(document).on("keyup", "input", function (e) {
+    $(document).on("keyup", "#text", function (e) {
         if(e.which == 13) {
             start();
         }
